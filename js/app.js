f700afa55b84e42938e77f7ba87f34497d324d54
FRESCH-Branding <info@freschbranding.nl>
2022-11-29 14:24:33 +0100

    redesign Stijlvol Interieur

--- js/app.js
+++ js/app.js
@@ -15,18 +15,4 @@ function close() {
   openMenu.style.display = "block";
 }
 
-
-// slider
-const wrapper = document.querySelector('.referentie-wrapper');
-const indicators = [...document.querySelectorAll('.indicators button')];
-
-let currentTestimonial = 0; // Default 0
-
-indicators.forEach((item, i) => {
-    item.addEventListener('click', () => {
-        indicators[currentTestimonial].classList.remove('active');
-        wrapper.style.marginLeft = `-${100 * i}%`;
-        item.classList.add('active');
-        currentTestimonial = i;
-    })
-})
+//carousel
