513680f0b524f512b1710bbdab518448b0fa5ed4
FRESCH-Branding <info@freschbranding.nl>
2023-02-28 15:25:12 +0100

    add composer and changed contact form

---
+++ form-to-email-fout.php
@@ -0,0 +1,40 @@
+<?php
+
+// $to_email = 'freek.bosch@outlook.com';
+// $subject = 'Testing PHP Mail';
+// $message = 'This mail is sent using the PHP mail function';
+// $headers = 'From: freek.bosch@outlook.com';
+// mail($to_email,$subject,$message,$headers);
+
+if (isset($_POST['email'])) {
+    $errors = '';
+    $myemail = 'info@outlook.com'; // <----- Put your email address here
+    $name = $_POST['name'];
+    $email_address = $_POST['email'];
+    $message = $_POST['message'];
+    $phone = $_POST['number'];
+    $subject = $_POST['subject'];
+    
+    if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email_address)) {
+        $errors .= "\n Error: Invalid email address";
+    }
+
+    if (empty($errors)) {
+        $to = $myemail;
+        $email_subject = "Contact form submission: $name";
+        $email_body =   "You have received a new message. "." Here are the details:\n Name: $name \n 
+                        "."Email: $email_address\n Message \n $message";
+        $headers = "From: $myemail\n";
+        $headers .= "Reply-To: $email_address";
+        mail(
+            $to, 
+            $email_subject,
+            $email_body,
+            $headers
+        );
+
+        // redirect to the 'contact' page
+        header('Location: contact.html');
+    }
+}
+?>
--- form-to-email.php
+++ form-to-email.php
@@ -1,25 +1,52 @@
 <?php
-if(isset($_POST['email'])) {
-    $errors = '';
-    $myemail = 'info@freekvandenbosch.nl'; // <----- Put your email address here
-    $name = $_POST['name'];
-    $email_address = $_POST['email'];
-    $message = $_POST['message'];
-    $phone = $_POST['number'];
-    $subject = $_POST['subject'];
-    if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email_address)) {
-        $errors .= "\n Error: Invalid email address";
-    }
-    if( empty($errors)) {
-        $to = $myemail;
-        $email_subject = "Contact form submission: $name";
-        $email_body =   "You have received a new message. "." Here are the details:\n Name: $name \n 
-                        "."Email: $email_address\n Message \n $message";
-        $headers = "From: $myemail\n";
-        $headers .= "Reply-To: $email_address";
-        mail($to,$email_subject,$email_body,$headers);
-        // redirect to the 'contact' page
-        header('Location: contact.html');
-    }
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.mijndomein.nl';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'info@stijlvolinterieur.nl';                     //SMTP username
+    $mail->Password   = '*4SYsi9RUm#Gbd3';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('info@stijlvolinterieur.nl', $_POST['naam']);
+    $mail->addAddress('info@stijlvolinterieur.nl');     //Add a recipient
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $_POST['onderwerp'];
+    $mail->Body    = 
+        $_POST['naam'].'<br>'.
+        $_POST['telefoonnummer'].'<br>'.
+        $_POST['email'].'<br>'.
+        '<br><br>'.
+        $_POST['bericht'];
+    
+    $mail->AltBody = $_POST['bericht'];
+
+    $mail->send();
+
+    header('Location: contact.html');
+    
+} catch (Exception $e) {
+    echo "Bericht kon niet worden verzonden. Mailer Error: {$mail->ErrorInfo}";
 }
 ?>
